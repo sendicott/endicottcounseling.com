2c9f36f15e8e0832a7532406de42e00c3b805121
Scott Endicott <sendicott@redventures.com>
2022-03-28 16:51:53 -0400

    more gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,24 @@
-// Compile sass into CSS & auto-inject into browsers
-// task('build', function () {
-//   return gulp
-//     .src(["themes/endicott-counseling/scss/styles.scss"])
-//     .pipe(sass())
-//     .pipe(gulp.dest("themes/endicott-counseling/source/assets/css"));
-// });
-
-// Move the javascript files into our /src/js folder
-// gulp.task('js', function() {
-//     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-//         .pipe(gulp.dest("src/js"));
-//     });
-
-// Static Server + watching scss/html files
-// watch("themes/endicott-counseling/scss/**/*.scss", gulp.series("sass"));
-
-// gulp.task("dev", gulp.series("watch"));
-
-// gulp.task("build", gulp.series("sass"));
-
-
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const compileSass = require('gulp-sass');
+const minifyCss = require('gulp-clean-css');
+const sourceMaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
 
 compileSass.compiler = require('node-sass');
 
 const bundleSass = () => {
   return src('themes/endicott-counseling/scss/styles.scss')
+    .pipe(sourceMaps.init())
     .pipe(compileSass().on('error', compileSass.logError))
+    .pipe(minifyCss())
+    .pipe(sourceMaps.write())
+    .pipe(concat('bundle.css'))
     .pipe(dest('themes/endicott-counseling/source/assets/css'));
+};
+
+const bundleJs = () => {
+  return src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(dest('src/js'));
 };
 
 const devWatch = () => {
@@ -36,4 +26,5 @@ const devWatch = () => {
 };
 
 exports.bundleSass = bundleSass;
+exports.bundleJs = bundleJs;
 exports.devWatch = devWatch;
